e779eddf35919367bc72b741f2a1f23e46df712f
Rαηյαη Kumαr <hunt6289@gmail.com>
2025-04-27 09:24:35 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Ubuntu from "../components/ubuntu";
 import ReactGA from 'react-ga4';
 import Meta from "../components/SEO/Meta";
 
-const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID;
-ReactGA.initialize(TRACKING_ID);
+if (process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID) {
+  ReactGA.initialize(process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID);
+}
 
 function App() {
   return (
